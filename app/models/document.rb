c4e1bef29a88b36182be2e7431783ab2d9d8d8ee
Erdinc Atilgan <erdinc.atilgan@gmail.com>
2015-07-31 23:27:47 -0400

    document uploads saved to database

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ActiveRecord::Base
 
 	belongs_to :lab
 
-	validates :name, :data, presence: true
+	validates :name, :data, :lab_id, presence: true
 
 	# mount_uploader :doc_file, AttachmentUploader
 
